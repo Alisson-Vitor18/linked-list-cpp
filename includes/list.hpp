8fb42e53024755eccc05bbfaff2d52f9ce14d2b2
Alisson-Vitor18 <alissonvpb18@gmail.com>
2026-05-15 23:22:32 -0300

    Criei a estrutura do header list.hpp

---
+++ includes/list.hpp
@@ -0,0 +1,24 @@
+#pragma once 
+#include <memory>
+
+template<typename T>
+class List {
+    private:
+        struct Node {
+            T value;
+            std::unique_ptr<Node> next;
+            std::unique_ptr<Node> prev;
+
+            //Construtor de Node
+            List(const T& value, std::unique_ptr<Node> next = nullptr,
+                std::unique_ptr<Node> prev = nullptr;);
+        };
+        std::unique_ptr<Node> head;
+        std::unique_ptr<Node> tail;
+        std::size_t size;
+
+    public:
+        //Construtor da classe List
+        List() = default;
+
+};  
